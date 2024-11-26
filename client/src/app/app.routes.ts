81b3ae01c5a7f07c7100ec17f2f9375f57bd79fc
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-11-26 12:59:09 +0200

    Add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './user/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '/users/register', component: RegisterComponent}
+];
