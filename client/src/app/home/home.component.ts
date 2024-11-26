590ba563dedb71a5670eda41171c49a9901d7309
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-11-26 11:59:20 +0200

    Add base HTML for components

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Animals } from '../types/Animal';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  forAdopt: Animals[] = [];
+  foundAndLost: Animals[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getLatestAdopt().subscribe((forAdopt) => {
+      this.forAdopt = forAdopt;
+    });
+    this.apiService.lostAndFoundLatest().subscribe((foundAndLost) => {
+      this.foundAndLost = foundAndLost;
+    })
+  }
 }
